662be524ba3ab8deca2f6af52527c4f1f6833915
Kelli Kirk <kelli.kirk@voco.ee>
2024-12-01 13:29:29 +0200

    Update the Todo by id

--- controllers/todos.js
+++ controllers/todos.js
@@ -19,29 +19,38 @@ class todoController {
             newTask: newTodo
         })
     } 
-    // meetod, mis otsib massiivis identifikaatori järgi ülesande
+    
     updateTodo(req, res){
-        // võtab urli parameetritest
-        const todoId = req.params.id
+        const todoId = req.params.id;
+        console.log("Received todoId from URL:", todoId);
+        
         // võtab uuendatud ülesande nime
-        const updatedTask = req.body.task
-        // võtab massiivist elemendi indeksi, kui todo id on sama, mis url params id
-        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+        const updatedTask = req.body.task;
+        
+        // otsib massiivist todo objekti
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId);
+        console.log("PATCH received for id:", todoId);
+        console.log("Updated task:", updatedTask);
+        console.log("todo array before update:", this.TODOS);
+    
         // kui url params ei ole õige, saadab veateate
-        if (todoIndex < 0) {
-            throw new Error('Could not find todo');
+        if (todoIndex === -1) {
+            return res.status(404).json({ message: 'Could not find todo' });
         }
+    
+        console.log("Found todo at index:", todoIndex);
         
         // Kui id on korras, todo uuendatakse
         // Uuendamiseks luuakse element sama id ja uue ülesandega ning salvestatakse samasse massiivi
-        this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask)
+        this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask);
         
         // Näitab uuendatud infot
         res.json({
             message: 'Updated todo',
             updatedTask: this.TODOS[todoIndex] 
-        })
+        });
     }  
+     
     
     // meetod, mis tagastab JSON väljundisse kõik massiivi lisatud ülesanded
     getTodos(req, res) {
